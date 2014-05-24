a50749cfa8157ec98338dea8782ea99b0dcca8b7
David Klassen <f0rk.tt@gmail.com>
2014-05-24 21:10:52 +0700

    Backend WIP

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,77 @@
 var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
+var users = {};
 
-app.listen(8080);
+function handler(req, res) {
+  fs.readFile(__dirname + '/index.html', function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading index.html');
-      }
-
-      res.writeHead(200);
-      res.end(data);
-    });
+    res.writeHead(200);
+    res.end(data);
+  });
 }
 
-var users = {};
+function updateUserList(users) {
+  for (var user in users) {
+    users[user].socket.emit('user-list', Object.keys(users).filter(function (u) { return u !== user; }));
+  }
+}
+
+function createFight(sourceUser, targetUser) {
+  users[sourceUser].fight = {
+    user: targetUser,
+    health: 100
+  };
+
+  users[targetUser].fight = {
+    user: sourceUser,
+    health: 100
+  };
+}
 
 io.sockets.on('connection', function (socket) {
   var username;
 
   socket.on('handshake', function (data) {
     username = data.username;
-    users[username] = socket;
-    for (var user in users) {
-      users[user].emit('user-list', Object.keys(users));
-    }
+    users[username] = { socket: socket };
+    updateUserList(users);
   });
 
   socket.on('disconnect', function () {
     delete users[username];
-    for (var user in users) {
-      users[user].emit('user-list', Object.keys(users));
-    }
+    updateUserList(users);
   });
 
   socket.on('fight-request', function (user) {
-    console.log(username + ' requests a fight with ' + user);
+    users[user].socket.emit('fight-request', username);
+  });
+
+  socket.on('accept-request', function (user) {
+    users[user].socket.emit('accept-request', username);
+    createFight(user, username);
+  });
+
+  socket.on('hit', function (data) {
+    if (users[username].fight) {
+      var targetUser = users[username].fight.user;
+      users[targetUser].fight.health -= data / 1000;
+
+      if (users[targetUser].fight.health > 0) {
+        users[targetUser].socket.emit('your-health', users[targetUser].fight.health);
+        users[username].socket.emit('enemy-health', users[targetUser].fight.health);
+      } else {
+        users[targetUser].socket.emit('loose');
+        users[username].socket.emit('win');
+        delete users[targetUser].fight;
+        delete users[username].fight;
+      }
+    }
   });
 });
+
+app.listen(8080);
