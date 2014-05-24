1fca6a8fbe4a9a20c3ec0f3aee4ec69a86b37158
David Klassen <f0rk.tt@gmail.com>
2014-05-24 19:54:31 +0700

    New backend

--- backend/app/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function (app) {
-    app.set('log-level', 0);
-    app.set('port', 3333);
-    app.set('secret', 'KJFdfsfuir&dfasf7');
-};
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,43 @@
-'use strict';
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
 
-var server = require('./app/app').server;
-var app = require('./app/app').app;
+app.listen(8080);
 
-server.listen(app.get('port'), function () {
-    console.log('Server running on ' + server.address().address + ':' + server.address().port);
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+    });
+}
+
+var users = {};
+
+io.sockets.on('connection', function (socket) {
+  var username;
+
+  socket.on('handshake', function (data) {
+    username = data.username;
+    users[username] = socket;
+    for (var user in users) {
+      users[user].emit('user-list', Object.keys(users));
+    }
+  });
+
+  socket.on('disconnect', function () {
+    delete users[username];
+    for (var user in users) {
+      users[user].emit('user-list', Object.keys(users));
+    }
+  });
+
+  socket.on('fight-request', function (user) {
+    console.log(username + ' requests a fight with ' + user);
+  });
 });
